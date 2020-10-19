612b843fac32b64abe1c3fea700c8008e62c9d37
januszpolak <janusz.polak@interia.pl>
2020-10-19 09:28:16 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Radio_Internetowe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.playRadioBtn = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(47, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(314, 147);
+            this.listBox1.TabIndex = 0;
+            // 
+            // playRadioBtn
+            // 
+            this.playRadioBtn.BackColor = System.Drawing.Color.Lime;
+            this.playRadioBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.playRadioBtn.Location = new System.Drawing.Point(47, 176);
+            this.playRadioBtn.Name = "playRadioBtn";
+            this.playRadioBtn.Size = new System.Drawing.Size(314, 58);
+            this.playRadioBtn.TabIndex = 1;
+            this.playRadioBtn.Text = "Graj Radio !!!";
+            this.playRadioBtn.UseVisualStyleBackColor = false;
+            this.playRadioBtn.Click += new System.EventHandler(this.playRadioBtn_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(47, 240);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(314, 113);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(408, 384);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.playRadioBtn);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Radio internetowe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button playRadioBtn;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio_Internetowe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("RMF FM");
+            listBox1.Items.Add("RMF MAXXX");
+            listBox1.Items.Add("Radio Kraków");
+            listBox1.Items.Add("Mansfield 103.2");
+        }
+
+        private void playRadioBtn_Click(object sender, EventArgs e)
+        {
+            string url = string.Empty;
+
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    url = "http://80.48.65.98:/rmf_fm";
+                    break;
+                case 1:
+                    url = "http://217.74.72.6:/rmf_maxxx";
+                    break;
+                case 2:
+                    url = "http://stream4.nadaje.com:9680/radiokrakow-s3";
+                    break;
+                case 3:
+                    url = "http://live.audiospace.co/mansfield1032";
+                    break;
+                default:
+                    MessageBox.Show("Wybierz stację");
+                    break;
+            }
+
+            if (!url.Equals(""))
+            {
+                axWindowsMediaPlayer1.URL = url;
+            }
+        }
+    }
+}
