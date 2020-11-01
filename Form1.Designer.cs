ce8d9de7dc8ff23d6a340de02ff2fcc786c614be
januszpolak <janusz.polak@interia.pl>
2020-11-01 20:13:29 +0100

    update app look

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,55 +29,90 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.playRadioBtn = new System.Windows.Forms.Button();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonRmf = new System.Windows.Forms.Button();
+            this.buttonMaxx = new System.Windows.Forms.Button();
+            this.buttonKrakow = new System.Windows.Forms.Button();
+            this.buttonMansfield = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.BackColor = System.Drawing.Color.Tan;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(138, 110);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(179, 147);
-            this.listBox1.TabIndex = 0;
-            // 
-            // playRadioBtn
-            // 
-            this.playRadioBtn.BackColor = System.Drawing.Color.AliceBlue;
-            this.playRadioBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.playRadioBtn.Location = new System.Drawing.Point(398, 296);
-            this.playRadioBtn.Name = "playRadioBtn";
-            this.playRadioBtn.Size = new System.Drawing.Size(74, 48);
-            this.playRadioBtn.TabIndex = 1;
-            this.playRadioBtn.Text = "Play";
-            this.playRadioBtn.UseVisualStyleBackColor = false;
-            this.playRadioBtn.Click += new System.EventHandler(this.playRadioBtn_Click);
             // 
             // axWindowsMediaPlayer1
             // 
             this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(452, 12);
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(54, 91);
             this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(154, 52);
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(53, 52);
             this.axWindowsMediaPlayer1.TabIndex = 2;
             this.axWindowsMediaPlayer1.Visible = false;
             // 
-            // button2
+            // buttonRmf
             // 
-            this.button2.BackColor = System.Drawing.Color.AliceBlue;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(509, 296);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 48);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Stop";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.buttonRmf.Location = new System.Drawing.Point(385, 191);
+            this.buttonRmf.Name = "buttonRmf";
+            this.buttonRmf.Size = new System.Drawing.Size(63, 23);
+            this.buttonRmf.TabIndex = 5;
+            this.buttonRmf.Text = "rmf fm";
+            this.buttonRmf.UseVisualStyleBackColor = true;
+            this.buttonRmf.Click += new System.EventHandler(this.buttonRmf_Click);
+            // 
+            // buttonMaxx
+            // 
+            this.buttonMaxx.Location = new System.Drawing.Point(385, 220);
+            this.buttonMaxx.Name = "buttonMaxx";
+            this.buttonMaxx.Size = new System.Drawing.Size(63, 23);
+            this.buttonMaxx.TabIndex = 7;
+            this.buttonMaxx.Text = "rmf maxx";
+            this.buttonMaxx.UseVisualStyleBackColor = true;
+            this.buttonMaxx.Click += new System.EventHandler(this.buttonMaxx_Click);
+            // 
+            // buttonKrakow
+            // 
+            this.buttonKrakow.Location = new System.Drawing.Point(385, 249);
+            this.buttonKrakow.Name = "buttonKrakow";
+            this.buttonKrakow.Size = new System.Drawing.Size(63, 23);
+            this.buttonKrakow.TabIndex = 8;
+            this.buttonKrakow.Text = "Kraków";
+            this.buttonKrakow.UseVisualStyleBackColor = true;
+            this.buttonKrakow.Click += new System.EventHandler(this.buttonKrakow_Click);
+            // 
+            // buttonMansfield
+            // 
+            this.buttonMansfield.Location = new System.Drawing.Point(385, 278);
+            this.buttonMansfield.Name = "buttonMansfield";
+            this.buttonMansfield.Size = new System.Drawing.Size(63, 23);
+            this.buttonMansfield.TabIndex = 9;
+            this.buttonMansfield.Text = "Mansfield";
+            this.buttonMansfield.UseVisualStyleBackColor = true;
+            this.buttonMansfield.Click += new System.EventHandler(this.buttonMansfield_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(474, 262);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("OCR-B 10 BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(385, 127);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 58);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
@@ -85,11 +120,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(795, 425);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(567, 387);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonMansfield);
+            this.Controls.Add(this.buttonKrakow);
+            this.Controls.Add(this.buttonMaxx);
+            this.Controls.Add(this.buttonRmf);
             this.Controls.Add(this.axWindowsMediaPlayer1);
-            this.Controls.Add(this.playRadioBtn);
-            this.Controls.Add(this.listBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -98,16 +136,20 @@
             this.Text = "Radio internetowe";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button playRadioBtn;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonRmf;
+        private System.Windows.Forms.Button buttonMaxx;
+        private System.Windows.Forms.Button buttonKrakow;
+        private System.Windows.Forms.Button buttonMansfield;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace Radio_Internetowe
 {
     public partial class Form1 : Form
     {
+
+        string url = string.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -19,44 +21,49 @@ namespace Radio_Internetowe
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            listBox1.Items.Add("RMF FM");
-            listBox1.Items.Add("RMF MAXXX");
-            listBox1.Items.Add("Radio Kraków");
-            listBox1.Items.Add("Mansfield 103.2");
+           
         }
 
-        private void playRadioBtn_Click(object sender, EventArgs e)
+       
+
+        private void buttonRmf_Click(object sender, EventArgs e)
         {
-            string url = string.Empty;
-
-            switch (listBox1.SelectedIndex)
-            {
-                case 0:
-                    url = "http://80.48.65.98:/rmf_fm";
-                    break;
-                case 1:
-                    url = "http://217.74.72.6:/rmf_maxxx";
-                    break;
-                case 2:
-                    url = "http://stream4.nadaje.com:9680/radiokrakow-s3";
-                    break;
-                case 3:
-                    url = "http://live.audiospace.co/mansfield1032";
-                    break;
-                default:
-                    MessageBox.Show("Wybierz stację");
-                    break;
-            }
-
-            if (!url.Equals(""))
-            {
-                axWindowsMediaPlayer1.URL = url;
-            }
+           
+            url = "http://80.48.65.98:/rmf_fm";
+            axWindowsMediaPlayer1.URL = url;
+            textBox1.Text = "RMF FM 96.0 Mhz";
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void buttonMaxx_Click(object sender, EventArgs e)
+        {
+            
+            url = "http://217.74.72.6:/rmf_maxxx";
+            axWindowsMediaPlayer1.URL = url;
+            textBox1.Text = "RMF MAXXX 96.7 Mhz";
+        }
+
+        private void buttonKrakow_Click(object sender, EventArgs e)
+        {
+           
+            url = "http://stream4.nadaje.com:9680/radiokrakow-s3";
+            axWindowsMediaPlayer1.URL = url;
+            textBox1.Text = "RADIO KRAKÓW 101.6 Mhz";
+
+        }
+
+        private void buttonMansfield_Click(object sender, EventArgs e)
+        {
+           
+            url = "http://live.audiospace.co/mansfield1032";
+            axWindowsMediaPlayer1.URL = url;
+            textBox1.Text = "RADIO MANSFIELD 103.2 Mhz";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
             axWindowsMediaPlayer1.close();
         }
+
+    
     }
 }
